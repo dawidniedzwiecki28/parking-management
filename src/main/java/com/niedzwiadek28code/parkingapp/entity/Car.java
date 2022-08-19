a1ae5f08d5261a8e373930740b04e675b1b97c7a
dawidniedzwiecki28 <dawidniedzwiecki28@gmail.com>
2022-08-19 18:45:25 +0200

    commit

--- src/main/java/com/niedzwiadek28code/parkingapp/entity/Car.java
+++ src/main/java/com/niedzwiadek28code/parkingapp/entity/Car.java
@@ -2,7 +2,6 @@ package com.niedzwiadek28code.parkingapp.entity;
 
 import lombok.Getter;
 import lombok.NoArgsConstructor;
-import lombok.RequiredArgsConstructor;
 import lombok.Setter;
 
 import javax.persistence.Entity;
@@ -12,7 +11,7 @@ import java.time.LocalDateTime;
 
 @Getter
 @Setter
-@RequiredArgsConstructor
+@NoArgsConstructor
 @Entity
 @Table
 public class Car {
@@ -24,6 +23,19 @@ public class Car {
     private LocalDateTime departureDate;
     private boolean timeUp;
     private boolean checker;
+
+    public Car(String registrationNumber, String country, LocalDateTime arrivalDate, boolean paymentStatus, LocalDateTime departureDate) {
+        this.registrationNumber = registrationNumber;
+        this.country = country;
+        this.arrivalDate = arrivalDate;
+        this.paymentStatus = paymentStatus;
+        this.departureDate = departureDate;
+    }
+
+    public Car(String registrationNumber, LocalDateTime arrivalDate) {
+        this.registrationNumber = registrationNumber;
+        this.arrivalDate = arrivalDate;
+    }
 
     public void toUpdate(Car sourceCar) {
         this.registrationNumber = sourceCar.registrationNumber;
